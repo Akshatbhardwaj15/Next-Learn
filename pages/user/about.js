8392c1814ca9751dd59639d524a8b082724bd203
Akshatbhardwaj15 <akshatbhardwaj680@gmail.com>
2024-10-16 22:18:03 +0530

    Added

---
+++ pages/user/about.js
@@ -0,0 +1,9 @@
+const UserIndex = () => {
+    return (
+        <div>
+            <h1>This is the user about</h1>
+        </div>
+    );
+};
+
+export default UserIndex;
---
+++ pages/user/index.js
@@ -0,0 +1,11 @@
+const { defaultConfig } = require("next/dist/server/config-shared");
+
+const UserIndex = () => {
+    return (
+        <div>
+            <h1> This is a user index</h1>
+        </div>
+    );
+};
+
+export default UserIndex;
